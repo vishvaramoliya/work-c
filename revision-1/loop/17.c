1b3d5197959cd2f03207d1f5a377cc19d690b4ad
vishvaramoliya <vishvaramoliya98@gmail.com>
2024-04-25 19:33:15 +0530

    rv1

--- revision-1/loop/17.c
+++ revision-1/loop/17.c
@@ -1,7 +1,21 @@
 // 17. Create a program to print the Fibonacci series up to a specified limit using a while loop.
 
-#include<stdio.h>
-main()
+#include <stdio.h>
+main() 
 {
-    
+  int i, n;
+  int t1 = 0, t2 = 1, nt = t1 + t2;
+
+  printf("Enter the number of terms: ");
+  scanf("%d", &n);
+
+  printf("Fibonacci Series: %d, %d, ", t1, t2);
+
+  for (i = 3; i <= n; ++i) {
+    printf("%d, ", nt);
+    t1 = t2;
+    t2 = nt;
+    nt = t1 + t2;
+  }
+
 }
